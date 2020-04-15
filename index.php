0e90627a236312537a88e5b18dc70b958008a93d
dsijakovski98 <dsijakovski98@github.com>
2020-04-15 12:48:35 +0200

    index.php pocetna faza

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <title>QuoRating - Rate stuff</title>
+</head>
+
+<body>
+<?php
+    include_once 'utils/navbar.php';    
+?>
+
+
+<!-- LAYOUT -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h3 class="text-center">Rate your favorite movies from home</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3 sidebar bg-dark text-center">
+            <p class="text-light">Sidebar Links</p>
+        </div>
+        <div class="col-md-9 content bg-light">
+            <p>Main Content</p>
+        </div>
+    </div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
