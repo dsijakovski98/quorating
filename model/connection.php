11a38ad491041a867ce2707f3a9b050d8c069425
tjovcheska <jovcheskateodora@gmail.com>
2020-04-15 13:52:57 +0200

    signinSignup

---
+++ model/connection.php
@@ -0,0 +1,16 @@
+<?php
+//Set database connection
+$dbHost = "localhost";
+$dbUser = "root";
+$dbPassword = "";
+$dbName = "quorating";
+
+try {
+  $dsn = "mysql:host=" . $dbHost . ";dbname=" . $dbName;
+  $pdo = new PDO($dsn, $dbUser, $dbPassword);
+} catch(PDOException $e) {
+  error_message = $e->getMessage();
+    include('errors/db_error_connect.php');
+    exit();
+}
+?>
---
+++ signup_user_db.php
@@ -0,0 +1,43 @@
+<?php 
+include ("model/connection.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $user_name = $_POST['user_name'];
+  $user_email = $_POST['user_email'];
+  $user_pass = $_POST['user_pass'];
+  $user_gender = $_POST['user_gender'];
+
+  if($user_name=='')
+  {
+      echo "<script>alert('We can not verify your name')</script>";
+  }
+  else if (strlen($user_pass)<8) 
+  {
+      echo "<script>alert('Password should be minimum 8 characters!')</script>";
+      exit();
+  }
+  else {
+
+      $sql = "INSERT INTO users (user_name, user_pass,user_email, user_gender) VALUES (:user_name, :user_pass, :user_email,:user_gender)";
+
+      $stmt = $pdo->prepare($sql);
+      
+      $stmt->execute(['user_name' => $user_name, 'user_pass' => $user_pass, 'user_email' => $user_email,'user_gender'=>$user_gender]);
+
+      if($sql)
+	    {
+		    echo "<script>alert('Congratulations $user_name, your account has been created sucessfully!')</script>";
+		    echo "<script>window.open('signin.php','_self')</script>";
+	    }
+	    else
+	    {
+		    echo "<script>alert('Registration failed, try again!')</script>";
+		    echo "<script>window.open('signup.php')</script>";
+	    }
+      $user_name = "";
+      $user_pass = "";
+      $user_email = "";
+	  $user_gender="";
+    }
+  }
+?>
