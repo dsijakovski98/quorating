d7c84fefafe5981f686112e240a8f6cb80966326
dsijakovski98 <dsijakovski98@github.com>
2020-04-16 20:54:44 +0200

    reverted to last stable push

--- quorating/utils/footer.php
+++
@@ -1,6 +0,0 @@
-<div class="container">
-    <hr style="background:black;">
-    <footer class="text-right">
-       <?php echo "<p>&copy;". date("Y") ."  QuoRating, Inc.</p>"; ?>
-    </footer>
-</div>
