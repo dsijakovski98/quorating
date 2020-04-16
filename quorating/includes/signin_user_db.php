d8da2dfbdae6f65c72bfab24c1bae5d58a9ffc1c
tjovcheska <jovcheskateodora@gmail.com>
2020-04-16 20:20:05 +0200

    poraka

---
+++ quorating/includes/signin_user_db.php
@@ -0,0 +1,38 @@
+<?php 
+session_start();
+
+require_once '../model/queries.php'; 
+
+	if(isset($_POST['sign_in'])){
+		$uid = htmlentities($_POST['email']);
+		$pass = htmlentities($_POST['pass']);
+
+
+		// Get the user from the 'users' table VIA email
+		$q = new Queries();
+		
+		$sql = "SELECT * FROM users WHERE user_email = ? OR user_name = ?";
+		$params = array($uid, $uid);
+
+		$result = $q->query($sql, $params);
+		
+		$check_user = $result->rowCount();
+		$row = $result->fetch();
+		$user_password = $row['user_pass'];
+		// Check if password is correct
+		$pwd = password_verify($pass, $user_password);
+
+		if($check_user == 1 && $pwd == 1) {
+			// $_SESSION['user_email'] = $email;
+			header("Location: ../index.php");
+		}
+		else if($check_user == 1) { // Password incorrect
+			header("Location: ../signin.php?error=pwd");
+			exit();
+		}
+		else { // User/email incorrect
+			header("Location: ../signin.php?error=user");
+			exit();
+		}
+	}
+	?>
