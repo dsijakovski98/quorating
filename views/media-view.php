00afd6997b30e2f07efc5438a2d767df861f4b21
tjovcheska <jovcheskateodora@gmail.com>
2020-04-19 20:03:25 +0200

    total view media-view.php

--- views/media-view.php
+++ views/media-view.php
@@ -18,8 +18,9 @@ require_once '../control/media-view-control.php';
 	<head>
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1" user-scalable=no>
         <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 		<!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
 		<title><?php echo "$table Review | Review"; ?></title>
 		<link rel="stylesheet" href="../css/cssCategories/style.css">
@@ -37,10 +38,10 @@ require_once '../control/media-view-control.php';
     ?>
     <br>
         
-        <div class="content">
+        <div class="content" >
 			<div class="row">
 
-            <div class="col-md-6">
+            <div class="col-md-6" align="center">
 					<figure class="movie-poster"><img src="../images/thumb-3.jpg" alt="#"></figure>
                 </div>
                 
@@ -64,10 +65,68 @@ require_once '../control/media-view-control.php';
                     <li><strong>Rating:</strong> 
 							<div class="star-rating" title="Rated 4.00 out of 5"><span style="width:80%"><strong class="rating">4.00</strong> out of 5</span></div></li>
                     </ul>
+                    <table>
+                    
+                    <tr>
+                    <div align="center" class="bg-dark" padding: 100px; color:white;>
+                        <i class="far fa-lightbulb" data-index="0"></i>
+                        <i class="far fa-lightbulb" data-index="1"></i>
+                        <i class="far fa-lightbulb" data-index="2"></i>
+                        <i class="far fa-lightbulb" data-index="3"></i>
+                        <i class="far fa-lightbulb" data-index="4"></i>
+                    </tr>
+                    <br>
+                    <tr>
+                    <button class="btn btn-primary btn-block btn-lg bg-dark" style="height:50px;width:200px" name="goBack"><a href="media-list.php">Go Home</button>
+                    </tr>
+                    
+                    </table>
+                        <br><br>
+                    </div>
 
+                    <script src="http://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
+                    <script>
+                        var ratedIndex = -1, uID = 0;
+
+                        $(document).ready(function () {
+                        resetBulbColors();
+
+                        if (localStorage.getItem('ratedIndex') != null) {
+                        setBulbs(parseInt(localStorage.getItem('ratedIndex')));
+                        uID = localStorage.getItem('uID');
+                        }
+
+                        $('.fa-lightbulb').on('click', function () {
+                        ratedIndex = parseInt($(this).data('index'));
+                        localStorage.setItem('ratedIndex', ratedIndex);
+                        saveToTheDB();
+                        });
+
+                        $('.fa-lightbulb').mouseover(function () {
+                        resetBulbColors();
+                        var currentIndex = parseInt($(this).data('index'));
+                        setBulbs(currentIndex);
+                        });
+
+                        $('.fa-lightbulb').mouseleave(function () {
+                        resetBulbColors();
+
+                        if (ratedIndex != -1)
+                        setBulbs(ratedIndex);
+                        });
+                    });
+
+                    function setBulbs(max) {
+                    for (var i=0; i <= max; i++)
+                        $('.fa-lightbulb:eq('+i+')').css('color', 'yellow');
+                    }
+
+                    function resetBulbColors() {
+                    $('.fa-lightbulb').css('color', 'white');
+                    }
+    </script>
 				</div><!--.col-md-6-->
             </div> <!-- .row -->
-            <button class="btn btn-primary btn-block btn-lg bg-dark" style="height:50px;width:200px" name="goBack"><a href="media-list.php">Go Home</button>
         </div><!-- .content-->
         
 		<?php
